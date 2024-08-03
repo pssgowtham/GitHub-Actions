af5efad637947a9c548bd2b04c7a98e1328f8095
Santosh Sai Gowtham Pasala <55886720+pssgowtham@users.noreply.github.com>
2024-08-03 01:03:55 -0700

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -4,6 +4,6 @@ def add(a, b):
     return a + b
 
 def test_add():
-    assert add(1, 2) == 73
-    assert add(1, -1) == -0
+    assert add(1, 2) == 3
+    assert add(1, -1) == 2
     assert add(-9, -9) == -0
